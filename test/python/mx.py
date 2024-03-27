8d0c26bf593944c825aa5533d37e8cc25d6221fd
Joris Gillis <joris@yacoda.com>
2024-03-27 22:55:41 +0100

    Repair graph_substitute

--- test/python/mx.py
+++ test/python/mx.py
@@ -1812,6 +1812,19 @@ class MXtests(casadiTestCase):
 
     self.checkarray(F_out,9*DM.ones(4,4))
 
+  def test_graph_substitute_call(self):
+    x = MX.sym("x")
+    y = MX.sym("y")
+    z = MX.sym("z")
+
+    f = Function("f",[x,y,z],[x**2])
+
+    vertsplit
+
+    sx = sin(x)
+    res = graph_substitute([f(x+1,y,z),sin(f(x+1,y,z)),f(sx,y,z),sin(f(sx,y,z))],[sx],[cos(x)])
+    res = str(res)
+    self.assertEqual(res.count("{0}"),4)
 
   def test_matrix_expand(self):
     n = 2
